703ae429a943a0711adbca5f933961017b3c39f8
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-03-20 10:20:03 -0600

    Test workoutschema

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000
-
+const db = require("./models");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +17,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", {
 
 // routes
 app.use(require("./routes/api.js"));
+
+// Creation of a test workoutSchema
+db.workout.create({name: 'test workout'})
+  .then(dbWorkout => {
+    console.log('dbWorkout: ', dbWorkout);
+  })
+  .catch(({message}) => {
+    console.log('message: ', message);
+  })
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
