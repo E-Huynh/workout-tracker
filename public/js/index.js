0609a21252569799754989b430d7dcb15981a8fd
Erik Huynh <Erik.W.Huynh@gmail.com>
2020-03-22 13:25:42 -0600

    change index.html to landing page

--- public/js/index.js
+++ public/js/index.js
@@ -1,26 +1,15 @@
 
 $(document).ready(function () {
-    // create new workout
-    $('#workoutForm').on('submit', function(event) {
-        event.preventDefault();
-        const workoutName = {name: $('#workoutInput').val().toLowerCase()};
-        $.post('/api/workout', workoutName, function(APIdata){
-            //console.log('data: ', APIdata)
-            $('#workoutInput').val('')
-            //DO SOMETHING WITH THE DATA
-        })
-        .catch(err => {
-            err.responseJSON && err.responseJSON.message ? console.log(err.responseJSON.message) : console.log(err.responseJSON.errmsg)
-        });
-    });
-    // Finds last workout
-    $('#lastWorkout').on('click', function(event) {
-        event.preventDefault();
+    $('#viewLastBtn').on('click', function() {
+        console.log('viewLastBtn was clicked');
         $.get('/api/workout', function(data) {
             console.log('data: ', data);
             //DO SOMETHING WITH THE DATA
         }).catch(err => {
             console.log('err: ', err);
         })
+    });
+    $('#createNewBtn').on('click', function() {
+        console.log('createNewBtn was clicked');
     });
 })
---
+++ public/js/workout.js
@@ -0,0 +1,26 @@
+
+$(document).ready(function () {
+    // create new workout
+    $('#workoutForm').on('submit', function(event) {
+        event.preventDefault();
+        const workoutName = {name: $('#workoutInput').val().toLowerCase()};
+        $.post('/api/workout', workoutName, function(APIdata){
+            console.log('data: ', APIdata)
+            $('#workoutInput').val('')
+            //DO SOMETHING WITH THE DATA
+        })
+        .catch(err => {
+            err.responseJSON && err.responseJSON.message ? console.log(err.responseJSON.message) : console.log(err.responseJSON.errmsg)
+        });
+    });
+    // Finds last workout
+    $('#lastWorkout').on('click', function(event) {
+        event.preventDefault();
+        $.get('/api/workout', function(data) {
+            console.log('data: ', data);
+            //DO SOMETHING WITH THE DATA
+        }).catch(err => {
+            console.log('err: ', err);
+        })
+    });
+})
